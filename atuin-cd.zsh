5af7bdd0392c03aa96c3161c4a57f9f8b342dd50
jerem-punk <jerem@punkpunkpunk.org>
2026-05-29 18:10:11 +0200

    print last visit time in blue

--- atuin-cd.zsh
+++ atuin-cd.zsh
@@ -1,13 +1,16 @@
 atuin-cd() {
   local db="${ATUIN_DB:-$HOME/.local/share/atuin/history.db}"
 
-  sqlite3 -separator ' ' "$db" '
+  sqlite3 "$db" '
     SELECT
+      char(27) || "[34m" ||
       strftime(
         "%Y-%m-%d",
         MAX(timestamp) / 1000000000,
         "unixepoch"
-      ) || " " || cwd
+      ) ||
+      char(27) || "[0m " ||
+      cwd
     FROM history
     WHERE cwd IS NOT NULL
       AND cwd != ""
