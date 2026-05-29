84deba0b6d90cdf08e761561c36cfc0b43d8048a
jerem-punk <jerem@punkpunkpunk.org>
2026-05-29 18:04:26 +0200

    added last directory visit date in front of the directory name

--- atuin-cd.zsh
+++ atuin-cd.zsh
@@ -1,11 +1,17 @@
 atuin-cd() {
   local db="${ATUIN_DB:-$HOME/.local/share/atuin/history.db}"
 
-  sqlite3 "$db" '
-    SELECT DISTINCT cwd
+  sqlite3 -separator ' ' "$db" '
+    SELECT
+      strftime(
+        "%Y-%m-%d",
+        MAX(timestamp) / 1000000000,
+        "unixepoch"
+      ) || " " || cwd
     FROM history
     WHERE cwd IS NOT NULL
       AND cwd != ""
-    ORDER BY timestamp DESC;
+    GROUP BY cwd
+    ORDER BY MAX(timestamp) DESC;
   '
 }
