ad12a1bbce4051ccd9eb06c2c14d2c63a5c24c1d
jerem-punk <jerem@punkpunkpunk.org>
2026-08-03 00:39:48 +0200

    calling the widget when editing a line inserts the selected directory path instead of cd to it

--- atuin-cd.plugin.zsh
+++ atuin-cd.plugin.zsh
@@ -8,45 +8,47 @@ fzf_preview_layout() {
   fi
 }
 
-atuin-cd() {
+atuin-select-dir() {
   local db="${ATUIN_DB:-$HOME/.local/share/atuin/history.db}"
-
-  local selected_dir
-
-  selected_dir="$(
-    sqlite3 "$db" "
-      SELECT
-        char(27) || '[34m' ||
-        strftime('%Y-%m-%d', MAX(timestamp) / 1000000000, 'unixepoch') ||
-        char(27) || '[0m'
-        || ' ' ||
-        replace(cwd, '$HOME', '~')
-        || char(9) ||
-        cwd
-      FROM history
-      WHERE cwd IS NOT NULL
-        AND cwd != ''
-      GROUP BY cwd
-      ORDER BY MAX(timestamp) DESC;
-    " | fzf \
-      --ansi \
-      --no-sort \
-      --delimiter=$'\t' \
-      --with-nth=1 \
-      --accept-nth=2 \
-      --preview 'eza -lah --icons --color=always {2}' \
-      --preview-window "$(fzf_preview_layout)"
-  )"
-
-  [[ -n "$selected_dir" ]] && cd "$selected_dir"
+  sqlite3 "$db" "
+    SELECT
+      char(27) || '[34m' ||
+      strftime('%Y-%m-%d', MAX(timestamp) / 1000000000, 'unixepoch') ||
+      char(27) || '[0m'
+      || ' ' ||
+      replace(cwd, '$HOME', '~')
+      || char(9) ||
+      cwd
+    FROM history
+    WHERE cwd IS NOT NULL
+      AND cwd != ''
+    GROUP BY cwd
+    ORDER BY MAX(timestamp) DESC;
+  " | fzf \
+    --ansi \
+    --no-sort \
+    --delimiter=$'\t' \
+    --with-nth=1 \
+    --accept-nth=2 \
+    --preview 'eza -lah --icons --color=always {2}' \
+    --preview-window "$(fzf_preview_layout)"
 }
 
 atuin-cd-widget() {
-  atuin-cd
-  zle reset-prompt
+  local selected_dir
+  local was_empty=$(( ${#BUFFER} == 0 ))
+
+  selected_dir="$(atuin-select-dir)"
+  [[ -z "$selected_dir" ]] && zle redisplay && return 0
+
+  if (( was_empty )); then
+    cd -- "$selected_dir"
+    zle reset-prompt
+  else
+    LBUFFER+="${(q)selected_dir}"
+  fi
 }
 
 zle -N atuin-cd-widget
 bindkey '^[d' atuin-cd-widget
-
 
